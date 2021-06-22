e6893c1f9eeb588c04679fb3b42951746937295d
Viniciuslb <viniciuslb067@gmail.com>
2021-06-22 20:28:30 -0300

    feat: authentication

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
-import googleIconImg from "../assets/images/google-icon.svg";
 
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
 
 export function NewRoom() {
+  // const { user } = useAuth();
+ 
   return (
     <div id="page-auth">
       <aside>
@@ -24,7 +28,7 @@ export function NewRoom() {
             <input type="text" placeholder="Nome da sala" />
             <Button type="submit">Criar sala</Button>
           </form>
-          <p>Quer entrar em uma sala existente? <a href="#">clique aqui</a></p>
+          <p>Quer entrar em uma sala existente? <Link to="/">clique aqui</Link></p>
         </div>
       </main>
     </div>
